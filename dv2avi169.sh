8c1f71541fb490be6ddd2cc963cba0e3fb5834ed
Mandar Joshi <mandar37@gmail.com>
2014-05-26 17:43:22 +0530

    Create dv2avi169.sh

---
+++ dv2avi169.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+  echo "Needs two args."
+  exit 0
+fi
+
+ffmpeg -i "$1" -target dvd "$2".mpg
+mencoder "$2".mpg -oac mp3lame -lameopts cbr:br=64:vol=1 -ovc lavc -lavcopts vhq:vcodec=mpeg4:aspect=16/9 -vf pp=lb -o "$2"final.avi
